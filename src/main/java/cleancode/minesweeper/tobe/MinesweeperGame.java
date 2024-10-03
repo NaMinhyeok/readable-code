8224e98c454f692c0a02d526b5cc7edde0c243ad
NaMinhyeok <nmh9097@gmail.com>
2024-10-03 13:25:14 +0900

    section4: ISP
    - 인터페이스가 하나의 책임만 가지는게 맞는지 생각하기

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -1,13 +1,14 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelebel.Beginner;
 import cleancode.minesweeper.tobe.gamelebel.GameLevel;
-import cleancode.minesweeper.tobe.gamelebel.VeryBeginner;
 
 public class MinesweeperGame {
     public static void main(String[] args) {
-        GameLevel gameLevel = new VeryBeginner();
+        GameLevel gameLevel = new Beginner();
 
         Minesweeper minesweeper = new Minesweeper(gameLevel);
+        minesweeper.initialize();
         minesweeper.run();
     }
 
