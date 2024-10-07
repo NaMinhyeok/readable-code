9289706f520271fcc958a5313ae5ae35c2c290e8
NaMinhyeok <nmh9097@gmail.com>
2024-10-07 21:44:55 +0900

    section5: config로 미래를 위해 추상화하기
    - 미래를 생각해서 추상화한다
    - 너무 미래를 위하면 오버엔지니어링이됨

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,5 +1,6 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.gamelebel.Beginner;
 import cleancode.minesweeper.tobe.gamelebel.GameLevel;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
@@ -9,11 +10,14 @@ import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
     public static void main(String[] args) {
-        GameLevel gameLevel = new Beginner();
-        InputHandler inputHandler = new ConsoleInputHandler();
-        OutputHandler outputHandler = new ConsoleOutputHandler();
 
-        Minesweeper minesweeper = new Minesweeper(gameLevel,inputHandler,outputHandler);
+        GameConfig gameConfig = new GameConfig(
+            new Beginner(),
+            new ConsoleInputHandler(),
+            new ConsoleOutputHandler()
+        );
+
+        Minesweeper minesweeper = new Minesweeper(gameConfig);
         minesweeper.initialize();
         minesweeper.run();
     }
